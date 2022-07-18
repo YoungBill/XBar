0314dca59dc80b98ebf337326a77a53b6b802d4f
taochen <935612713@qq.com>
2022-07-18 12:17:52 +0800

    [M] 优化BarUtils代码结构

--- XBar/src/main/java/com/ct/douyu/xbar/BarUtils.kt
+++ XBar/src/main/java/com/ct/douyu/xbar/BarUtils.kt
@@ -12,7 +12,7 @@ import androidx.fragment.app.FragmentActivity
  * Email: 935612713@qq.com.
  */
 @RequiresApi(Build.VERSION_CODES.KITKAT)
-fun FragmentActivity.statusBarTransparent(statusBarLight: Boolean) {
+fun FragmentActivity.transparentStatusBar(statusBarLight: Boolean) {
     when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
             window.isStatusBarContrastEnforced = false
@@ -27,11 +27,11 @@ fun FragmentActivity.statusBarTransparent(statusBarLight: Boolean) {
                 window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         }
     }
-    setSystemUiFlagWithStatusBar(statusBarLight)
+    setStatusBarLightMode(statusBarLight)
 }
 
 @RequiresApi(Build.VERSION_CODES.KITKAT)
-fun FragmentActivity.navigationBarTransparent(navigationBarLight: Boolean) {
+fun FragmentActivity.transparentNavigationBar(navigationBarLight: Boolean) {
     when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
             window.isNavigationBarContrastEnforced = false
@@ -46,30 +46,35 @@ fun FragmentActivity.navigationBarTransparent(navigationBarLight: Boolean) {
                 window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
         }
     }
-    setSystemUiFlagWithNavigationBar(navigationBarLight)
+    setNavigationBarLightMode(navigationBarLight)
 }
 
 @RequiresApi(Build.VERSION_CODES.KITKAT)
-internal fun FragmentActivity.setSystemUiFlagWithStatusBar(statusBarLight: Boolean) {
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return
+fun FragmentActivity.setStatusBarLightMode(statusBarLight: Boolean) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        return
+    }
     window?.decorView?.systemUiVisibility = systemUiFlagWithStatusBar(statusBarLight)
 }
 
 @RequiresApi(Build.VERSION_CODES.KITKAT)
-internal fun FragmentActivity.setSystemUiFlagWithNavigationBar(navigationBarLight: Boolean) {
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return
+fun FragmentActivity.setNavigationBarLightMode(navigationBarLight: Boolean) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        return
+    }
     window?.decorView?.systemUiVisibility = systemUiFlagWithNavigationBar(navigationBarLight)
 }
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 private fun systemUiFlagWithStatusBar(statusBarLight: Boolean): Int {
-
     var flag = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
             or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
     when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
-            if (statusBarLight) flag = flag or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            if (statusBarLight) {
+                flag = flag or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
         }
     }
     return flag
@@ -77,13 +82,14 @@ private fun systemUiFlagWithStatusBar(statusBarLight: Boolean): Int {
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 private fun systemUiFlagWithNavigationBar(navigationBarLight: Boolean): Int {
-
     var flag = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
             or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
     when {
         Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
-            if (navigationBarLight) flag = flag or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            if (navigationBarLight) {
+                flag = flag or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
         }
     }
     return flag
